31dccad0fe2cc863fbe57e73aac0d3235c3e6778
qkrdkwl9090 <qkrdkwl9090@naver.com>
2020-04-23 23:22:30 +0900

    add GridXY function(todayWeather)

--- TodayWeather/app/src/main/java/com/dodong/todayweather/MainActivity.kt
+++ TodayWeather/app/src/main/java/com/dodong/todayweather/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.dodong.todayweather
 
 import android.app.Activity
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.facebook.stetho.Stetho
 import com.facebook.stetho.okhttp3.StethoInterceptor
 import okhttp3.OkHttpClient
@@ -14,42 +14,133 @@ import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.simplexml.SimpleXmlConverterFactory
 
+
 class MainActivity : AppCompatActivity() {
+    var TO_GRID = 0
+    var TO_GPS = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tmp = convertGRID_GPS(TO_GRID, 37.579871128849334, 126.98935225645432)
+        val tmp2 = convertGRID_GPS(TO_GRID, 35.101148844565955, 129.02478725562108)
+        val tmp3 = convertGRID_GPS(TO_GRID, 33.500946412305076, 126.54663058817043)
+
+        Log.e("test1", "x = " + tmp!!.x + ", y = " + tmp!!.y)
+        Log.e("test1", "x = " + tmp2!!.x + ", y = " + tmp2!!.y)
+        Log.e("test1", "x = " + tmp3!!.x + ", y = " + tmp3!!.y)
         getData(this)
         Stetho.initializeWithDefaults(this)
     }
-}
 
-fun getData(activity: Activity){
-    val client = OkHttpClient.Builder()
-        .addNetworkInterceptor(StethoInterceptor())
-        .build()
-    val retrofit = Retrofit.Builder()
-        .baseUrl("http://www.kma.go.kr/")
-        .addConverterFactory(SimpleXmlConverterFactory.create())
-        .client(client)
-        .build()
-    val service =retrofit.create(RetrofitService::class.java)
+    fun getData(activity: Activity) {
+        val client = OkHttpClient.Builder()
+            .addNetworkInterceptor(StethoInterceptor())
+            .build()
+        val retrofit = Retrofit.Builder()
+            .baseUrl("http://www.kma.go.kr/")
+            .addConverterFactory(SimpleXmlConverterFactory.create())
+            .client(client)
+            .build()
+        val service = retrofit.create(RetrofitService::class.java)
 
-    service.getWeatherData().enqueue(object : Callback<weatherData>{
-        override fun onFailure(call: Call<weatherData>, t: Throwable) {
-            Log.d("test1", t.toString())
-            Toast.makeText(activity, "통신 실패", Toast.LENGTH_LONG).show()
-        }
-
-        override fun onResponse(
-            call: Call<weatherData>,
-            response: Response<weatherData>
-        ) {
-            Log.d("test1", "응답 성공")
-            if (response.isSuccessful) {
-                Log.d("test1", "응답 성공")
+        service.getWeatherData().enqueue(object : Callback<weatherData> {
+            override fun onFailure(call: Call<weatherData>, t: Throwable) {
+                Log.d("test1", t.toString())
+                Toast.makeText(activity, "통신 실패", Toast.LENGTH_LONG).show()
             }
-        }
 
-    })
+            override fun onResponse(
+                call: Call<weatherData>,
+                response: Response<weatherData>
+            ) {
+                Log.d("test1", "응답 성공")
+                if (response.isSuccessful) {
+                    Log.d("test1", "응답 성공")
+                }
+            }
+
+        })
+    }
+    private fun convertGRID_GPS(
+        mode: Int,
+        lat_X: Double,
+        lng_Y: Double
+    ): LatXLngY? {
+        val RE = 6371.00877 // 지구 반경(km)
+        val GRID = 5.0 // 격자 간격(km)
+        val SLAT1 = 30.0 // 투영 위도1(degree)
+        val SLAT2 = 60.0 // 투영 위도2(degree)
+        val OLON = 126.0 // 기준점 경도(degree)
+        val OLAT = 38.0 // 기준점 위도(degree)
+        val XO = 43.0 // 기준점 X좌표(GRID)
+        val YO = 136.0 // 기1준점 Y좌표(GRID)
+
+        //
+        // LCC DFS 좌표변환 ( code : "TO_GRID"(위경도->좌표, lat_X:위도,  lng_Y:경도), "TO_GPS"(좌표->위경도,  lat_X:x, lng_Y:y) )
+        //
+        val DEGRAD = Math.PI / 180.0
+        val RADDEG = 180.0 / Math.PI
+        val re = RE / GRID
+        val slat1 = SLAT1 * DEGRAD
+        val slat2 = SLAT2 * DEGRAD
+        val olon = OLON * DEGRAD
+        val olat = OLAT * DEGRAD
+        var sn = Math.tan(Math.PI * 0.25 + slat2 * 0.5) / Math.tan(Math.PI * 0.25 + slat1 * 0.5)
+        sn =Math.log(Math.cos(slat1) / Math.cos(slat2)) / Math.log(sn)
+        var sf = Math.tan(Math.PI * 0.25 + slat1 * 0.5)
+        sf = Math.pow(sf, sn) * Math.cos(slat1) / sn
+        var ro = Math.tan(Math.PI * 0.25 + olat * 0.5)
+        ro = re * sf / Math.pow(ro, sn)
+        val rs = LatXLngY()
+        if (mode == TO_GRID) {
+            rs.lat = lat_X
+            rs.lng = lng_Y
+            var ra =
+                Math.tan(Math.PI * 0.25 + lat_X * DEGRAD * 0.5)
+            ra = re * sf / Math.pow(ra, sn)
+            var theta = lng_Y * DEGRAD - olon
+            if (theta > Math.PI) theta -= 2.0 * Math.PI
+            if (theta < -Math.PI) theta += 2.0 * Math.PI
+            theta *= sn
+            rs.x = Math.floor(ra * Math.sin(theta) + XO + 0.5)
+            rs.y = Math.floor(ro - ra * Math.cos(theta) + YO + 0.5)
+        } else {
+            rs.x = lat_X
+            rs.y = lng_Y
+            val xn = lat_X - XO
+            val yn = ro - lng_Y + YO
+            var ra = Math.sqrt(xn * xn + yn * yn)
+            if (sn < 0.0) {
+                ra = -ra
+            }
+            var alat = Math.pow(re * sf / ra, 1.0 / sn)
+            alat = 2.0 * Math.atan(alat) - Math.PI * 0.5
+            var theta = 0.0
+            if (Math.abs(xn) <= 0.0) {
+                theta = 0.0
+            } else {
+                if (Math.abs(yn) <= 0.0) {
+                    theta = Math.PI * 0.5
+                    if (xn < 0.0) {
+                        theta = -theta
+                    }
+                } else theta = Math.atan2(xn, yn)
+            }
+            val alon = theta / sn + olon
+            rs.lat = alat * RADDEG
+            rs.lng = alon * RADDEG
+        }
+        return rs
+    }
+
+
+    internal class LatXLngY {
+        var lat = 0.0
+        var lng = 0.0
+        var x = 0.0
+        var y = 0.0
+    }
+
 }
