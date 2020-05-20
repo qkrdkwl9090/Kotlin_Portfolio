90a7ca4f26cf6ddca34b5a112f84cfc13d6ff5c7
qkrdkwl9090 <qkrdkwl9090@naver.com>
2020-05-20 10:39:55 +0900

    add Layout

--- TubePlayer/app/src/main/java/com/dodong/tubeplayer/Tube_detail.kt
+++ TubePlayer/app/src/main/java/com/dodong/tubeplayer/Tube_detail.kt
@@ -3,13 +3,10 @@ package com.dodong.tubeplayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
-
+class Tube_detail : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
+        setContentView(R.layout.activity_tube_detail)
     }
 }
